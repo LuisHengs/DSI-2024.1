c4d5b0cea1db6abd6c006c7b72f2f842ffb8d94d
EdnizLeandro <147220920+EdnizLeandro@users.noreply.github.com>
2024-09-10 11:12:34 -0300

    pages/Create pages.dart
    Esse reuni todas as exportações em um único arquivo, ele facilita a manutenção e a importação de múltiplas páginas em outros arquivos.

---
+++ Food_Delivery-App/lib/pages/pages.dart
@@ -0,0 +1,10 @@
+export 'package:flutter_food_delivery_app/pages/delivery_progress_page.dart';
+
+export 'package:flutter_food_delivery_app/pages/payment_page.dart';
+
+export 'package:flutter_food_delivery_app/pages/food_page.dart';
+export 'package:flutter_food_delivery_app/pages/home_page.dart';
+export 'package:flutter_food_delivery_app/pages/login_or_register_page.dart';
+export 'package:flutter_food_delivery_app/pages/login_page.dart';
+export 'package:flutter_food_delivery_app/pages/register_page.dart';
+export 'package:flutter_food_delivery_app/pages/setting_page.dart';
